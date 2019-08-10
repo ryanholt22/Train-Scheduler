e243d80424abc362250c6bb37bcbe2a33f3d3269
Ryan Holt <ryanholt22@yahoo.com>
2019-08-10 02:18:11 -0500

    put spacers inbetween the input where the user enters info

---
+++ Assets/Javascript/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCGV5nd421AXKXz-sXEd4l1XoEOCbk28Hw",
+    authDomain: "train-scheduler-3b693.firebaseapp.com",
+    databaseURL: "https://train-scheduler-3b693.firebaseio.com",
+    projectId: "train-scheduler-3b693",
+    storageBucket: "",
+    messagingSenderId: "407548590888",
+    appId: "1:407548590888:web:721fee0adcea1563"
+  };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
