6210efbed2e44021b5a126f4f5ad29574b5c0030
Ryan Holt <ryanholt22@yahoo.com>
2019-08-13 19:00:44 -0500

    made changes to js, firebase is now working

--- Assets/Javascript/logic.js
+++ Assets/Javascript/logic.js
@@ -10,10 +10,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var inpt1 = document.getElementById("inpt1");
-var inpt2 = document.getElementById("inpt2");
-var inpt3 = document.getElementById("inpt3");
-var inpt4 = document.getElementById("inpt4");
+
 var tFrequency = 5;
 var firstTime = "06:30";
 var FirstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
@@ -32,13 +29,17 @@ console.log("Minutes till train ", minutesTillTrain);
 console.log("Next train arrives in " + moment(nextTrain).format("HH:mm"));
 
 
-$("button").on("click", function () {
+$("button").on("click", function input() {
   database.ref().set({
-    currTime: currentTime,
+    currTime: currentTime.format("HH:mm"),
     freq: tFrequency,
-    nextArrive: nextTrain,
+    nextArrive: nextTrain.format("HH:mm"),
     MinAway: minutesTillTrain
   });
+  var inpt1 = document.getElementById("inpt1");
+  var inpt2 = document.getElementById("inpt2");
+  var inpt3 = document.getElementById("inpt3");
+  var inpt4 = document.getElementById("inpt4");
+  console.log(inpt1, inpt2, inpt3, inpt4);
 });
-console.log(inpt1, inpt2, inpt3, inpt4);
 
